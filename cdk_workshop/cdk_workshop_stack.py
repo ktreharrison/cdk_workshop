a4ebe725f4f3200636b2ba07db47cb6bef49a8aa
Ken <ktreharrison@gmail.com>
2022-07-24 20:56:14 +0200

    Add deploy stage to the pipeline steps

--- cdk_workshop/cdk_workshop_stack.py
+++ cdk_workshop/cdk_workshop_stack.py
@@ -1,4 +1,5 @@
 from aws_cdk import Stack
+from aws_cdk import CfnOutput
 from aws_cdk import aws_apigateway as apigw
 from aws_cdk import aws_lambda as _lambda
 from cdk_dynamo_table_view import TableViewer
