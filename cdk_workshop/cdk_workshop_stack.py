c7b7a0fe04efdb543cbd7210040ff70e1865ec6b
Ken <ktreharrison@gmail.com>
2022-07-24 19:01:59 +0200

    Add Test & Pipline Stacks

--- cdk_workshop/cdk_workshop_stack.py
+++ cdk_workshop/cdk_workshop_stack.py
@@ -1,26 +1,39 @@
+from aws_cdk import Stack
+from aws_cdk import aws_apigateway as apigw
+from aws_cdk import aws_lambda as _lambda
+from cdk_dynamo_table_view import TableViewer
 from constructs import Construct
-from aws_cdk import (
-    Duration,
-    Stack,
-    aws_iam as iam,
-    aws_sqs as sqs,
-    aws_sns as sns,
-    aws_sns_subscriptions as subs,
-)
+
+from .hitcounter import HitCounter
 
 
 class CdkWorkshopStack(Stack):
-
+    # boilerplat code for Python CDK
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
-
-        queue = sqs.Queue(
-            self, "CdkWorkshopQueue",
-            visibility_timeout=Duration.seconds(300),
+        
+        # Defines an AWS Lambda resource
+        my_lambda = _lambda.Function(
+            self, 'HelloHandler',
+            runtime = _lambda.Runtime.PYTHON_3_7,
+            code = _lambda.Code.from_asset('lambda'),
+            handler = 'hello.handler',                                     
         )
-
-        topic = sns.Topic(
-            self, "CdkWorkshopTopic"
+        
+        # adding hit counter 
+        hello_with_counter = HitCounter(
+            self,
+            'HelloHitCounter',
+            downstream=my_lambda,
         )
-
-        topic.add_subscription(subs.SqsSubscription(queue))
+        
+        # Define an API Gateway resource 
+        apigw.LambdaRestApi(self, "Endpoint", handler=hello_with_counter._handler)
+        
+        # Using the aws cdk ddb table viewer
+        TableViewer(
+            self, 'ViewHitCounter',
+            title='Hello Hits',
+            table=hello_with_counter.table,
+            sort_by='-hits'
+        )
---
+++ lambda/hello.py
@@ -0,0 +1,12 @@
+import json
+
+
+def handler(event, context):
+    print(f"request: {json.dumps(event)}")
+    return {
+        'statusCode': 200,
+        'headers': {
+            'Content-Type': 'text/plain'
+        },
+        'body': f"Good Night Ken, Your lambda CDK has hit {event['path']}\n"
+    }
