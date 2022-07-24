2c834ae4683e4bbaff40c2eaa18d4af90992d2bf
Ken <ktreharrison@gmail.com>
2022-07-24 20:58:23 +0200

    Add deploy stage to the pipeline steps

--- cdk_workshop/pipeline_stack.py
+++ cdk_workshop/pipeline_stack.py
@@ -24,7 +24,7 @@ class WorkshopPipelineStack(Stack):
                 input=pipelines.CodePipelineSource.code_commit(repo, "main"),
                 commands=[
                     "npm install -g aws-cdk",  # Installs the cdk cli on Codebuild
-                    'pip install --upgrade pip'
+                    'pip install --upgrade pip',
                     "pip install -r requirements.txt",  # Instructs Codebuild to install required packages
                     "npx cdk synth",
                 ]
