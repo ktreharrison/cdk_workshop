ceb456da06d107aa3ee6a9f857af1e93e451654a
Ken <ktreharrison@gmail.com>
2022-07-24 19:54:09 +0200

    Add deploy stage to the pipeline steps

--- cdk_workshop/pipeline_stack.py
+++ cdk_workshop/pipeline_stack.py
@@ -7,32 +7,29 @@ from cdk_workshop.pipeline_stage import WorkshopPipelineStage
 
 
 class WorkshopPipelineStack(Stack):
-    # Python CDK Boilerplate Stack code
     def __init__(self, scope: Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
-    
-        # Create a CommitCode repository called 'WorkshopRepo'
-        repo = codecommit.Repository(self, 
-                'WorkshopRepo', repository_name = 'WorkshopRepo'
+
+        # Creates a CodeCommit repository called 'WorkshopRepo'
+        repo = codecommit.Repository(
+            self, "WorkshopRepo", repository_name="WorkshopRepo"
         )
-        
-        # Creating a pipline 
+
         pipeline = pipelines.CodePipeline(
-            self, 
-            'Pipeline', 
+            self,
+            "Pipeline",
             synth=pipelines.ShellStep(
-                'Synth', 
-                input=pipelines.CodePipelineSource.code_commit(repo,'main'),
+                "Synth",
+                input=pipelines.CodePipelineSource.code_commit(repo, "master"),
                 commands=[
-                    "npm install -g aws-cdk", # Installs the cdk cli on Codebuild
-                    "pip install -r requirements.txt", # Instructs Codebuild to install required packages
-                    'npx cdk synth'
-                          ]
-        
-            )
+                    "npm install -g aws-cdk",  # Installs the cdk cli on Codebuild
+                    "pip install -r requirements.txt",  # Instructs Codebuild to install required packages
+                    "npx cdk synth",
+                ]
+            ),
         )
-        # Adding a Stage to the pipline process 
-        deploy = WorkshopPipelineStage(self, 'Deploy')
+
+        deploy = WorkshopPipelineStage(self, "Deploy")
         deploy_stage = pipeline.add_stage(deploy)
             
          
